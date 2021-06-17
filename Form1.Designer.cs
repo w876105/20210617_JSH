4fe359bacbb60804897376625089c6c6562f7c76
w876105 <w876105@gmail.com>
2021-06-17 22:16:44 +0800

    上傳JSH的程式1

---
+++ Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace Integration整合
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timesLable = new System.Windows.Forms.Label();
+            this.panel0 = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.rowChooser = new System.Windows.Forms.NumericUpDown();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.GuessNumber = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button12 = new System.Windows.Forms.Button();
+            this.RangeNumber = new System.Windows.Forms.Label();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.panel0.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rowChooser)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(52, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(205, 104);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "①變數";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(375, 58);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(205, 104);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "②字串";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Font = new System.Drawing.Font("微軟正黑體", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.Location = new System.Drawing.Point(40, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(205, 36);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "③Mouse Click";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button4.Location = new System.Drawing.Point(39, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(202, 42);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "④畫半棵樹";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timesLable
+            // 
+            this.timesLable.AutoSize = true;
+            this.timesLable.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.timesLable.Location = new System.Drawing.Point(42, 50);
+            this.timesLable.Name = "timesLable";
+            this.timesLable.Size = new System.Drawing.Size(203, 38);
+            this.timesLable.TabIndex = 4;
+            this.timesLable.Text = "你愛我 ... 0 下";
+            // 
+            // panel0
+            // 
+            this.panel0.Controls.Add(this.button6);
+            this.panel0.Controls.Add(this.button5);
+            this.panel0.Controls.Add(this.button3);
+            this.panel0.Controls.Add(this.timesLable);
+            this.panel0.Location = new System.Drawing.Point(12, 198);
+            this.panel0.Name = "panel0";
+            this.panel0.Size = new System.Drawing.Size(293, 247);
+            this.panel0.TabIndex = 5;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button6.Location = new System.Drawing.Point(40, 180);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(205, 51);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "重來";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.Info;
+            this.button5.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button5.Location = new System.Drawing.Point(40, 112);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(205, 51);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "愛我";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // rowChooser
+            // 
+            this.rowChooser.Font = new System.Drawing.Font("Chiller", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rowChooser.Location = new System.Drawing.Point(141, 50);
+            this.rowChooser.Name = "rowChooser";
+            this.rowChooser.Size = new System.Drawing.Size(95, 39);
+            this.rowChooser.TabIndex = 6;
+            this.rowChooser.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.rowChooser);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Location = new System.Drawing.Point(336, 198);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(276, 247);
+            this.panel1.TabIndex = 7;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.Info;
+            this.button8.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button8.Location = new System.Drawing.Point(39, 181);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(187, 51);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "錢樹";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button7.Location = new System.Drawing.Point(39, 112);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(187, 51);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "松樹";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(19, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 38);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "要多高";
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button9.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button9.Location = new System.Drawing.Point(52, 481);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(205, 104);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "⑤九九乘法";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button10.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button10.Location = new System.Drawing.Point(362, 481);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(205, 104);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "⑥交換";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // GuessNumber
+            // 
+            this.GuessNumber.AcceptsReturn = true;
+            this.GuessNumber.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.GuessNumber.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.GuessNumber.Location = new System.Drawing.Point(91, 61);
+            this.GuessNumber.Name = "GuessNumber";
+            this.GuessNumber.Size = new System.Drawing.Size(100, 43);
+            this.GuessNumber.TabIndex = 10;
+            this.GuessNumber.Text = "0";
+            this.GuessNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button13);
+            this.panel2.Controls.Add(this.button12);
+            this.panel2.Controls.Add(this.RangeNumber);
+            this.panel2.Controls.Add(this.button11);
+            this.panel2.Controls.Add(this.GuessNumber);
+            this.panel2.Location = new System.Drawing.Point(697, 58);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(279, 387);
+            this.panel2.TabIndex = 11;
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button12.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button12.Location = new System.Drawing.Point(40, 0);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(202, 42);
+            this.button12.TabIndex = 10;
+            this.button12.Text = "⑦終極密碼";
+            this.button12.UseVisualStyleBackColor = false;
+            // 
+            // RangeNumber
+            // 
+            this.RangeNumber.AutoSize = true;
+            this.RangeNumber.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.RangeNumber.Font = new System.Drawing.Font("微軟正黑體", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.RangeNumber.Location = new System.Drawing.Point(58, 231);
+            this.RangeNumber.Name = "RangeNumber";
+            this.RangeNumber.Size = new System.Drawing.Size(120, 48);
+            this.RangeNumber.TabIndex = 12;
+            this.RangeNumber.Text = "A ~ B";
+            this.RangeNumber.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button11.Location = new System.Drawing.Point(91, 134);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(100, 50);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "猜吧";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.SystemColors.Info;
+            this.button13.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button13.Location = new System.Drawing.Point(91, 320);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(100, 50);
+            this.button13.TabIndex = 13;
+            this.button13.Text = "重來";
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1006, 639);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel0);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Integration整合";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel0.ResumeLayout(false);
+            this.panel0.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rowChooser)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label timesLable;
+        private System.Windows.Forms.Panel panel0;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.NumericUpDown rowChooser;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.TextBox GuessNumber;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Label RangeNumber;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button13;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Integration整合
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int Dilbert = 790621, Valentine = 20131030, WeddingDay = 20201010;
+
+            //int Dilbert = 790621;
+            //int Valentine = 20131030;
+            //int WeddingDay = 20201010;
+
+            MessageBox.Show( " Dilbert = " + Dilbert);
+            MessageBox.Show( " Valentine = " + Valentine);
+            MessageBox.Show( " WeddingDay = " + WeddingDay);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string Zero = " 這是一張小紙條 ";
+            string One = " 準備好要看了嗎？ ";
+            string Two = " 哈囉！小胖 " + "\n" + " 有句話想跟你說 ";
+            string Three = " 謝謝你，非常謝謝你 " + "\r\n" + " 來到我的生命裡。" ;
+            string Four = " 陪伴我，豐富我的日常。 ";
+            string Five = " 這句話，我很少對你說，" + "但是 ";
+            string Six = " This is what I have been doing ";
+            string Sevem = " 我愛你 ";
+
+            MessageBox.Show(Zero);
+            MessageBox.Show(One);
+            MessageBox.Show(Two);
+            MessageBox.Show(Three);
+            MessageBox.Show(Four);
+            MessageBox.Show(Five);
+            MessageBox.Show(Six);
+            MessageBox.Show(Sevem);
+        }
+        int times = 0;
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            times = times + 1;
+            //times ++ ;
+            //times +=1 ;
+
+            timesLable.Text = " 你愛我 ... " + times + " 下 ";
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            times = 0;
+
+            timesLable.Text = " 你愛我 ... " + times + " 下 ";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Draw(int row, string mark)
+        {
+            //int row = (int)rowChooser.Value;     →可合併的寫法，把 "Draw" 寫成 Method (方法)
+            string result = "";
+
+            for (int i = 1; i <= row; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    result += mark;
+                }
+                result = result + "\n";
+            }
+            MessageBox.Show(result);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int row = (int)rowChooser.Value;
+            Draw(row, "\\");
+            //string result = "";
+
+            //for ( int i = 1; i <= row; i++)
+            //{
+            //    for (int j = 1; j <= i ; j++)
+            //    {
+            //        result += "^";
+            //    }
+            //    result = result + "\n";
+            //}
+            //MessageBox.Show(result);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int row = (int)rowChooser.Value;
+            Draw(row, "$");
+            //string result = "";  →可合併的寫法，把 "Draw" 寫成 Method (方法)
+
+            //for (int i = 1; i <= row; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        result += "$";
+            //    }
+            //    result = result + "\n";
+            //}
+            //MessageBox.Show(result);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            string text = "";
+
+            for ( int A = 1; A <= 9; A++)
+            {
+                for ( int B = 1; B <= 9; B++)
+                {
+                    text = text +  A + "x" + B + "=" + A * B;
+                }
+                text += "\n";
+            }
+            MessageBox.Show(text);
+        }
+        
+        private void button10_Click(object sender, EventArgs e)
+        {
+            int a = 3;
+            int b = 5;
+            MessageBox.Show("a=" + a + ",b=" + b);
+
+            
+
+            //something.....
+            int c = a;//c=3
+            a = b;//a=5
+            b = c;
+            //.............
+            MessageBox.Show("a=" + a + ",b=" + b);
+
+        }
+
+        int A = 0, B = 50, ans = 28;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RangeNumber.Text = A + " ～ " + B;
+            panel0.BorderStyle = BorderStyle.FixedSingle;
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel2.BorderStyle = BorderStyle.FixedSingle; 
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            int guess = int.Parse(GuessNumber.Text);
+
+            if(guess == ans)
+            {
+                MessageBox.Show(" Bingo！ ");
+                MessageBox.Show(" 再玩一次？ ");
+                A = 0;
+                B = 50;
+                RangeNumber.Text = A + " ～ " + B;
+                GuessNumber.Text = " 0 ";
+            }
+            else if (guess < ans)
+            {
+                A = guess;
+                RangeNumber.Text = A + " ～ " + B;
+
+            }
+            else if (guess > ans)
+            {
+                B = guess;
+                RangeNumber.Text = A + " ～ " + B;
+            }
+        }
+        private void button13_Click(object sender, EventArgs e)
+        {
+            A = 0;
+            B = 50;
+            RangeNumber.Text = A + " ～ " + B;
+            GuessNumber.Text = " 0 ";
+        }
+
+    }
+}
